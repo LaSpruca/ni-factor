4544896695d0c612738204eacfe6463b1e72b035
Nathan Hare <me@laspruca.nz>
2022-03-08 22:38:27 +1300

    We do a small quantity of modifications

--- src/app.rs
+++ src/app.rs
@@ -50,7 +50,7 @@ impl epi::App for TemplateApp {
 
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-    fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
+    fn update(&mut self, ctx: &egui::Context, _frame: &epi::Frame) {
         let Self {
             tokens,
             show_add_dialog,
@@ -62,16 +62,16 @@ impl epi::App for TemplateApp {
         // Tip: a good default choice is to just keep the `CentralPanel`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        frame.quit();
-                    }
-                });
-            });
-        });
+        // egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+        //     // The top panel is often a good place for a menu bar:
+        //     egui::menu::bar(ui, |ui| {
+        //         ui.menu_button("File", |ui| {
+        //             if ui.button("Quit").clicked() {
+        //                 frame.quit();
+        //             }
+        //         });
+        //     });
+        // });
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
             ui.heading("Side Panel");
@@ -83,7 +83,7 @@ impl epi::App for TemplateApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.with_layout(Layout::top_down(Align::LEFT), |ui| {
-                if tokens.is_empty() {
+                if !tokens.is_empty() {
                     for (index, token) in tokens.clone().iter().enumerate() {
                         ui.with_layout(Layout::top_down(Align::LEFT), |ui| {
                             ui.label(RichText::new("TOK EN!").size(40.0));
